c0c3b75b6d3222ebe324a5d24df03bf0d891cf62
CaptainMentallic <shaanjaswal01@gmail.com>
2026-06-20 16:41:44 -0600

    Small changes and removed google search scraping

--- scraper.py
+++ scraper.py
@@ -1,8 +1,12 @@
-# Scraper for finding components from external resources if not already in a database
+# Scraper for finding datasheet urls from external resources if not already in the database 
+
+# Scrape #1: DatasheetArchive
+# Scrape #2: ....
+# Scrape #3: ....
+
 
 #potential websites:
 #https://www.findchips.com/
-
 
 import requests
 from bs4 import BeautifulSoup
@@ -15,10 +19,12 @@ class ScrapedComponent(BaseModel):
 
 def fetch_datasheet_url(part_number: str) -> ScrapedComponent | None:
     part_number = part_number.upper()
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36"}
 
+    Scrape #1: DatasheetArchive
     try: 
         url = f"https://www.datasheetarchive.com/datasheet/{part_number}"
-        response = requests.get(url, headers={"User-Agent": "Mozilla/5.0"}, timeout=10)
+        response = requests.get(url, headers=headers, timeout=5)
         response.raise_for_status()  # Check if the request was successful
 
         soup = BeautifulSoup(response.text, "html.parser")
@@ -26,16 +32,33 @@ def fetch_datasheet_url(part_number: str) -> ScrapedComponent | None:
         a = soup.find('a', class_='download-datasheet')
         pdf_page = a['href']
 
-        res = requests.get(pdf_page, allow_redirects=True, headers={"User-Agent": "Mozilla/5.0"}, timeout=10)
+        res = requests.get(pdf_page, allow_redirects=True, headers=headers, timeout=5)
 
         if res.url:
             return ScrapedComponent(
                 datasheet_url=res.url,
                 source="datasheetarchive"
             )
-        else:
-            print(f"No datasheet found for {part_number} on DatasheetArchive.")
-        
+    except Exception as e:
+        print(f"Scrape #1 (DatasheetArchive) failed for {part_number}: {e}")
+
+
+
+    # Scrape #2: ....
+    try:
+        print('')
 
     except Exception as e:
-        print(f"Something went wrong fetching datasheet for {part_number}: {e}")
+        print(f"Scrape #2 () failed for {part_number}: {e}")
+
+
+
+
+    # Scrape #3: ....
+    try:
+        print('')
+
+    except Exception as e:
+        print(f"Scrape #3 () failed for {part_number}: {e}")
+
+    return None 
